658b495f33b55359d4f1ae411e8b92b41d705d91
FairoozAlamMahi <23-51878-2@student.aiub.edu>
2025-11-01 14:46:31 +0600

    added some stuff and pictures

--- practice.php
+++ practice.php
@@ -5,17 +5,29 @@
 </head>
 <body>
 
-<h1>This is a <br>Heading</h1>
+<h1 style = "color: blue; font-size: 100px; font-family:fantasy    ">This is a <br>Heading</h1>
 hello
 
 <strong>what</strong><br>
 <b>what</b><br>
-<mark>what</mark><br>
+<mark>what</mark><br> <!-- comment -->
 <del>what</del><br>
 <em>what</em><br>
 <ins>what</ins><br>
 <sub>what</sub><br>
 <sup>what</sup>
+
+
+<style>
+
+h1{
+    background-color:yellow;
+}
+
+</style>
+
+<img src="received_270589927347812.png">
+
 
 <br><a href="https://www.youtube.com/watch?v=PO-BM7Caf0A"> go to youtube </a>
 
