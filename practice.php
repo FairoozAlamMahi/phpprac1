8d0a5c0e1d91efb3cf7d87b33d024b991cff0364
FairoozAlamMahi <23-51878-2@student.aiub.edu>
2025-11-01 14:26:02 +0600

    learn

--- practice.php
+++ practice.php
@@ -5,8 +5,21 @@
 </head>
 <body>
 
-<h1>This is a Heading</h1>
+<h1>This is a <br>Heading</h1>
 hello
+
+<strong>what</strong><br>
+<b>what</b><br>
+<mark>what</mark><br>
+<del>what</del><br>
+<em>what</em><br>
+<ins>what</ins><br>
+<sub>what</sub><br>
+<sup>what</sup>
+
+<br><a href="https://www.youtube.com/watch?v=PO-BM7Caf0A"> go to youtube </a>
+
+
 
 </body>
 </html>
