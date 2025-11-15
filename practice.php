b5161c165350b04b7bc04ca40a5e834dd7863e95
FairoozAlamMahi <23-51878-2@student.aiub.edu>
2025-11-15 08:49:03 +0600

    Learned new things

--- practice.php
+++ practice.php
@@ -5,112 +5,29 @@
 </head>
 <body>
 
-<h1 style = "color: blue; font-size: 100px; font-family:fantasy    ">This is a <br>Heading</h1>
-hello
+<h1 style="background-color: pink; height: 70px; width:50%"> Hello </h1>
 
-<strong>what</strong><br>
-<b>what</b><br>
-<mark>what</mark><br> <!-- comment -->
-<del>what</del><br>
-<em>what</em><br>
-<ins>what</ins><br>
-<sub>what</sub><br>
-<sup>what</sup>
+<div>
+    Hello <br>
+    im mahi <br>
+    this is a centered form i hope <br>
+    in a div negro <br>
+    <input type="text"> <br>
+    <input type="password"> <br>
 
+</div>
+<a href="https://www.youtube.com/watch?v=DT_aD0lGDZE">This is yt </a>
 
 <style>
 
-h1{
-    background-color:yellow;
+div{
+    width: 300px;
+    margin: auto;
+    background-color: pink;
+    
 }
 
 </style>
-
-<img src="received_270589927347812.png">
-
-
-<br><a href="https://www.youtube.com/watch?v=PO-BM7Caf0A"> go to youtube </a>
-
-
-
-
-<center><h2>Store</h2></center>
-
-
-<table style=background-color:black>
-
-<tr style=background-color:grey>
-    <th>Sunday</th>
-    <th>Monmday</th>
-    <th>Tuesday</th>
-    <th>Wednesday</th>
-    <th>thursday</th>
-    <th>Friday</th>
-    <th>Saturday</th>
-</tr>
-<tr style="background-color:lightgrey;">
-    <td>Closed</td>
-    <td>Open</td>
-    <td>Open</td>
-    <td>Open</td>
-    <td>Open</td>
-    <td>Open</td>
-    <td>Open</td>
-</tr>
-</table>
-
-<h2>Unordered list</h2>
-
-<ul>
-
-<li>Coffee</li>
-<li>Tea</li>
-<li>Milk</li>
-
-</ul>
-
-<h2>Ordered List</h2>
-
-<ol>
-    <li>Coffee</li>
-    <li>Tea</li>
-    <li>Milk</li>
-</ol>
-
-<center>
-
-<h1>This is a form.. blood one</h1>
-
-
-Enter Name:
-<input type="text"><br>
-Enter Age:
-<input type="text"><br>
-Enter Blood Group(Type):
-<select>
-    <option>A</option>
-    <option>B</option>
-    <option>O</option>
-    <option>AB</option>
-</select>
-<select>
-    <option>+</option>
-    <option>-</option>
-</select>
-<br>
-Enter if you have any deseases:
-    <input type="checkbox"> HIV
-    <input type="checkbox"> HPV
-    <input type="checkbox"> Spooky Vaginosis
-    <input type="checkbox"> Herpes
-    <input type="checkbox"> Optimus Prime
-    <br>
-Enter Checkbox Material:
-    <input type="radio"; name="Gender";>A
-    <input type="radio"; name="Gender";>B
-    <input type="radio"; name="Gender";>C
-
-</center>
 
 
 </body>
